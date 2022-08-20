ef53b22167cd21f1c9d69932e49f2679b2b057a6
Yayo <andreshernandez@Yayos-MacBook-Pro.local>
2022-08-20 11:51:33 -0500

    Updates

---
+++ precache-manifest.7281de54f6b488fc524afc533e13962d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "728f04a2cc7def784a47",
+    "url": "/atlantia-beers.github.io/static/css/main.cfc40f02.chunk.css"
+  },
+  {
+    "revision": "728f04a2cc7def784a47",
+    "url": "/atlantia-beers.github.io/static/js/main.7b53b8f5.chunk.js"
+  },
+  {
+    "revision": "3dd84210b5e5c159673a",
+    "url": "/atlantia-beers.github.io/static/js/runtime~main.a70c7348.js"
+  },
+  {
+    "revision": "d72b52c56061600ac09c",
+    "url": "/atlantia-beers.github.io/static/css/2.730cf4f0.chunk.css"
+  },
+  {
+    "revision": "d72b52c56061600ac09c",
+    "url": "/atlantia-beers.github.io/static/js/2.2842c845.chunk.js"
+  },
+  {
+    "revision": "aede78e04aca5b58eb34be5cc11da67c",
+    "url": "/atlantia-beers.github.io/index.html"
+  }
+];
